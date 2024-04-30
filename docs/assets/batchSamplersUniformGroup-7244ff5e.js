49ba51dadc999cb6f0a46d5d025484c5158ca585
quyinggang <yinggang.qu@gmail.com>
2024-04-30 20:32:23 +0800

    refactor: docs

---
+++ docs/assets/batchSamplersUniformGroup-7244ff5e.js
@@ -0,0 +1,1 @@
+import{z as e}from"./index-e61fb7a8.js";import{U as o}from"./colorToUniform-8369d275.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
---
+++ docs/assets/gm-0ebf93b7.js
@@ -0,0 +1,1 @@
+const e=""+new URL("gm-94e48fc6.jpg",import.meta.url).href;export{e as G};
