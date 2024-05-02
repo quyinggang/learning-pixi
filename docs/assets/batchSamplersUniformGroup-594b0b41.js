89cc3ec5c6e623bed114badf5ec673e3afdcb8d0
quyinggang <yinggang.qu@gmail.com>
2024-05-02 14:57:57 +0800

    refactor: docs

--- docs/assets/batchSamplersUniformGroup-594b0b41.js
+++
@@ -1,1 +0,0 @@
-import{y as e}from"./index-88ee97f3.js";import{U as o}from"./TexturePool-dfa03c68.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
